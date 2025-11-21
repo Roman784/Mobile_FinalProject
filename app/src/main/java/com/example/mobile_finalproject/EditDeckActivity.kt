0be60c103459f2931fa40a0983f1e6d938612c21
Роман Мироненко <79697765+Roman784@users.noreply.github.com>
2025-11-21 16:24:22 +0300

    Add: edit deck activity

---
+++ app/src/main/java/com/example/mobile_finalproject/EditDeckActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mobile_finalproject
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class EditDeckActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_deck_activity)
+
+        val deck_id = intent.getIntExtra("DECK_ID", -1)
+
+        findViewById<TextView>(R.id.deck_id).text = deck_id.toString()
+    }
+}
--- app/src/main/java/com/example/mobile_finalproject/MainActivity.kt
+++ app/src/main/java/com/example/mobile_finalproject/MainActivity.kt
@@ -1,38 +1,38 @@
 package com.example.mobile_finalproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.mobile_finalproject.models.Deck
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        const val TAG = "DeckApp"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Старт")
+        var addDeckButton = findViewById<Button>(R.id.btnAddDeck)
+
+        // Add new deck.
+        addDeckButton.setOnClickListener {
+            openEditDeckActivity(-1);
+        }
+
         loadDecks()
     }
 
     private fun loadDecks() {
-        Log.d(TAG, "Начало загрузки данных...")
-
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 val response = RetrofitClient.apiService.getDecks()
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         val decks = response.body() ?: emptyList()
 
-                        displayDecksInConsole(decks)
+                        createDeckItems(decks)
                     }
                 }
             } catch (e: Exception) {
@@ -49,13 +49,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun displayDecksInConsole(decks: List<Deck>) {
-        if (decks.isEmpty()) {
-            return
-        }
+    private fun createDeckItems(decks: List<Deck>) {
+        val container = findViewById<LinearLayout>(R.id.decksContainer)
 
         decks.forEach { deck ->
-            Log.d(TAG, "Deck: id=${deck.id}, name='${deck.name}'")
+            val itemView = LayoutInflater.from(this)
+                .inflate(R.layout.deck_selection_item, container, false)
+
+            val textView = itemView.findViewById<TextView>(R.id.tvName)
+            textView.text = deck.name
+
+            val editButton = itemView.findViewById<ImageButton>(R.id.btnPencil)
+            editButton.setOnClickListener {
+                openEditDeckActivity(deck.id)
+            }
+
+            container.addView(itemView)
         }
+    }
+
+    private fun openEditDeckActivity(deckId: Int) {
+        val intent = Intent(this, EditDeckActivity::class.java)
+        intent.putExtra("DECK_ID", deckId)
+        startActivity(intent)
     }
 }
